07c14a8bcda8bf9549d4f31a2ecbb18e4fe1f738
G00405094 <G00405094@atu.ie>
2022-11-08 14:03:02 +0000

    Added loop

--- src/main/java/UsingTryCatch.java
+++ src/main/java/UsingTryCatch.java
@@ -3,19 +3,23 @@ import java.util.Scanner;
 public class UsingTryCatch {
     public static void main(String[] args)
     {
-        try {
-            System.out.println("Please enter a number between 0 - 9:");
-            Scanner myscan = new Scanner(System.in);
-            int myNum = myscan.nextInt();
-            if (myNum <= 9 && myNum > 0) {
-                System.out.println("You entered " + myNum);
-            } else {
-                System.out.println("Not a valid number");
-            }
-        } catch (Exception e) {
-            System.out.println("Invalid, please try again");
-        }
+        boolean repeat = true;
+            while(repeat) {
+                System.out.println("Please enter a number between 0 - 9:");
+                Scanner myscan = new Scanner(System.in);
+                    try {
+                        int myNum = myscan.nextInt();
+                        if (myNum <= 9 && myNum > 0) {
+                            System.out.println("You entered " + myNum);
+                            repeat = false;
+                        } else {
+                            System.out.println("Not a valid number");
+                        }
+                    } catch (Exception e) {
+                        System.out.println("Error caught");
 
+                    }
+                }
     }
 }
 
